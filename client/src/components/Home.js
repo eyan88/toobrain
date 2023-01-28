b947f27a4b5f11f2e42fc226bc53454530c6d425
eyan88 <yaneric.4d@gmail.com>
2023-01-28 11:51:52 -0700

    add getnotes to retrieve notes from mock api

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,49 @@
 import Note from './Note';
+import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
+const MOCK_URI = ''
+
 const Home = () => {
+    const [notes, setNotes] = useState([]);
+
+    const getNotes = async () => {
+        try {
+            await fetch(`${MOCK_URI}/api/notes`, {
+                method: 'GET',
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    const notes = []
+                    for (const key in data) {
+                        data[key].id = key;
+                        notes.push(data[key])
+                    }
+                    console.log(notes);
+                    setNotes(notes);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        getNotes();
+    }, []);
 
     return (
         <>
             <div className='p-6 grid grid-cols-1 gap-2 overflow-auto md:grid-cols-2 md:gap-5 lg:grid-cols-6 lg:gap-5'>
-                <Note />
-                <Note />
-                <Note />
-                <Note />
-                <Note />
+                {notes.map((note) => (
+                    <Note
+                        key={note.id}
+                        title={note.title}
+                        content={note.content}
+                    />
+                ))}
             </div>
         </>
     )
