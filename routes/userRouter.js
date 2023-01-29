0f2ced7be4f069d5e2413ddccb19f16ee3537bc4
eyan88 <yaneric.4d@gmail.com>
2023-01-29 16:06:48 -0700

    add user and note persistence with mongodb, auth verification

---
+++ routes/userRouter.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const userCtrl = require('../controllers/userCtrl');
+
+router.post('/register', userCtrl.registerUser);
+router.post('/login', userCtrl.loginUser);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
+const userRouter = require('./routes/userRouter');
+const noteRouter = require('./routes/noteRouter');
+const { default: mongoose } = require('mongoose');
 
+app.use(express.json());
 app.use(cors());
+app.use('/users', userRouter);
+app.use('/api/notes', noteRouter)
 
-app.use('/login', (req,res) => {
-    console.log("received login request");
-    // save req.body.email and req.body.password (hashed)
-    res.send({
-        token:'test123',
-    });
-});
+const URI = process.env.REACT_APP_MONGO_URI;
+
+mongoose.connect(
+    URI,
+    (err) => {
+        if (err) throw err;
+        console.log("connected to MongoDB");
+    }
+);
 
 app.listen(8080, () => {
     console.log('api is running on localhost:8080');
